16c43f152559497eeb8f7f368cb54303c40b5fae
Tony Zhang <tonyzhang617@gmail.com>
2018-06-23 15:33:04 -0400

    Remove commented code

--- core/src/com/tianyi/zhang/multiplayer/snake/states/client/MainGameState.java
+++ core/src/com/tianyi/zhang/multiplayer/snake/states/client/MainGameState.java
@@ -25,11 +25,6 @@ public class MainGameState extends GameState implements InputProcessor {
     private static final String TAG = MainGameState.class.getCanonicalName();
     private final ScheduledExecutorService executor;
     private final int clientId;
-//    private final List<Snapshot> snapshots;
-//    private final Object snapshotsLock;
-    private volatile boolean serverReady = false;
-//    private final int snakeId;
-    private long lastUpdateTime;
     private volatile int roundTripMs;
     private final ClientSnapshot snapshot;
 
@@ -84,99 +79,8 @@ public class MainGameState extends GameState implements InputProcessor {
                 }
             }
         });
-//        _app.getAgent().setListener(new Listener() {
-//            @Override
-//            public void received(Connection connection, Object object) {
-//                if (object instanceof byte[]) {
-//                    Packet.Update update = Client.parseReceived(object);
-//                    Packet.Update.PState serverState = update.getState();
-//
-//                    if (serverState == Packet.Update.PState.READY) {
-//                        List<Packet.Update.PSnake> pSnakes = update.getSnapshots(0).getSnakesList();
-//                        int[] snakeIds = new int[pSnakes.size()];
-//                        for (int i = 0; i < pSnakes.size(); ++i) {
-//                            snakeIds[i] = pSnakes.get(i).getId();
-//                        }
-//                        synchronized (snapshotsLock) {
-//                            snapshots.add(new Snapshot(snakeIds));
-//                            for (int i = 0; i < FUTURE_STATES; ++i) {
-//                                snapshots.add(snapshots.get(i).next());
-//                            }
-//                        }
-//
-//                        ScheduledExecutorService executor = Executors.newSingleThreadScheduledExecutor();
-//                        executor.scheduleAtFixedRate(new Runnable() {
-//                            @Override
-//                            public void run() {
-//                                sendUpdate();
-//                            }
-//                        }, Constants.CLIENT_SEND_EVERY_MS, Constants.CLIENT_SEND_EVERY_MS, TimeUnit.MILLISECONDS);
-//
-//                        serverReady = true;
-//                    } else if (serverState == Packet.Update.PState.GAME_IN_PROGRESS && serverReady) {
-//                        processServerPacket(update);
-//                    }
-//                }
-//            }
-//        });
-
         Gdx.app.debug(TAG, "Main game loaded");
     }
-
-//    private Snapshot getCurrentSnapshot() {
-//        synchronized (snapshotsLock) {
-//            return snapshots.get(snapshots.size() - FUTURE_STATES);
-//        }
-//    }
-
-//    private void nextStep() {
-//        synchronized (snapshotsLock) {
-//            int size = snapshots.size();
-//            snapshots.add(snapshots.get(size-1).next());
-//            if (size == PAST_STATES + 1 + FUTURE_STATES) {
-//                snapshots.remove(0);
-//            }
-//        }
-//    }
-//
-//    private void sendUpdate() {
-//        Snapshot currentSnapshot = getCurrentSnapshot();
-//        Snake thisSnake = currentSnapshot.getSnakeById(snakeId);
-//        _app.getAgent().send(Packet.Update.newBuilder().setState(Packet.Update.PState.GAME_IN_PROGRESS).addSnapshots(
-//                Packet.Update.PSnapshot.newBuilder().setStep(currentSnapshot.getStep()).addSnakes(
-//                        Packet.Update.PSnake.newBuilder().setId(snakeId).setInputId(thisSnake.INPUT_ID)
-//                                .setDirection(thisSnake.DIRECTION).build()).build()).build());
-//    }
-//
-//    private void processServerPacket(Packet.Update update) {
-//        Gdx.app.debug(TAG, "Update from server received");
-//        synchronized (snapshotsLock) {
-//            int size = snapshots.size(), firstStep = snapshots.get(0).getStep();
-//            List<Packet.Update.PSnapshot> pSnapshots = update.getSnapshotsList();
-//            int pSize = pSnapshots.size(), pFirstStep = pSnapshots.get(0).getStep();
-//            int offset = pFirstStep - firstStep, startIndex = offset >= 0 ? offset : 0;
-//            for (int i = startIndex, p = 0; i < size && p < pSize; ++i, ++p) {
-//                // Compare Snapshots with PSnapshots
-//                Snapshot snapshot = snapshots.get(i);
-//                Packet.Update.PSnapshot pSnapshot = pSnapshots.get(p);
-//                boolean shouldUpdate = false;
-//                for (int j = 0; j < pSnapshot.getSnakesCount(); ++j) {
-//                    Packet.Update.PSnake pSnake = pSnapshot.getSnakes(j);
-//                    Snake snake = snapshot.getSnakeById(j);
-//                    if (snake.INPUT_ID < pSnake.getInputId()) {
-//                        snapshot.updateDirection(j, pSnake.getDirection(), pSnake.getInputId());
-//                        if (snake.DIRECTION != pSnake.getDirection()) {
-//                            shouldUpdate = true;
-//                        }
-//                    }
-//                }
-//                if (shouldUpdate && i+1 < size) {
-//                    // TODO: Implement Client Prediction with an input queue
-//                    snapshots.set(i+1, snapshot.next());
-//                }
-//            }
-//        }
-//    }
 
     @Override
     public void render(float delta) {
@@ -193,21 +97,6 @@ public class MainGameState extends GameState implements InputProcessor {
             }
             Gdx.app.debug(TAG, builder.toString());
         }
-//        if (TimeUnit.NANOSECONDS.toMillis(System.nanoTime() - lastUpdateTime) > MOVE_EVERY_MS || lastUpdateTime == 0) {
-//            if (!serverReady) {
-//                super.render(delta);
-//            } else {
-//                nextStep();
-//                Snake snake = getCurrentSnapshot().getSnakeById(snakeId);
-//                StringBuilder builder = new StringBuilder();
-//                for (Integer i : snake.COORDS) {
-//                    builder.append(i.intValue());
-//                    builder.append(' ');
-//                }
-//                Gdx.app.debug(TAG, builder.toString());
-//            }
-//            lastUpdateTime = System.nanoTime();
-//        }
     }
 
     @Override
--- core/src/com/tianyi/zhang/multiplayer/snake/states/server/SVMainGameState.java
+++ core/src/com/tianyi/zhang/multiplayer/snake/states/server/SVMainGameState.java
@@ -20,16 +20,11 @@ import java.util.List;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
-import java.util.concurrent.atomic.AtomicInteger;
 
 public class SVMainGameState extends GameState implements InputProcessor {
     private static final String TAG = SVMainGameState.class.getCanonicalName();
-//    private final List<Snapshot> snapshots;
-//    private final Object snapshotsLock;
     private final ServerSnapshot serverSnapshot;
     private final ScheduledExecutorService executor;
-    private long lastUpdateTime;
-    private AtomicInteger inputId;
     private final long startTimestamp;
 
     /**
@@ -41,28 +36,6 @@ public class SVMainGameState extends GameState implements InputProcessor {
         super(app);
         Gdx.input.setInputProcessor(this);
         Gdx.graphics.setContinuousRendering(false);
-
-//        int[] snakeIds = new int[connectionIds.size()+1];
-//        // Server has snake ID 0
-//        snakeIds[0] = 0;
-//        for (int i = 1; i <= connectionIds.size(); ++i) {
-//            snakeIds[i] = connectionIds.get(i-1);
-//        }
-
-//        snapshotsLock = new Object();
-//        snapshots = new LinkedList<Snapshot>();
-//        synchronized (snapshotsLock) {
-//            snapshots.add(new Snapshot(snakeIds));
-//            for (int i = 0; i < FUTURE_STATES; ++i) {
-//                snapshots.add(snapshots.get(i).next());
-//            }
-//        }
-
-        // TODO: Implement intermediate state where server sends out first packet and waits for clients to reply READY
-//        _app.getAgent().send(buildFirstPacket());
-
-//        lastUpdateTime = 0;
-//        inputId = new AtomicInteger(0);
 
         int[] tmpIds = new int[connectionIds.size()+1];
         tmpIds[0] = 0;
@@ -98,22 +71,6 @@ public class SVMainGameState extends GameState implements InputProcessor {
         Gdx.app.debug(TAG, "Server main game loaded");
     }
 
-//    private Snapshot getCurrentSnapshot() {
-//        synchronized (snapshotsLock) {
-//            return snapshots.get(snapshots.size() - FUTURE_STATES);
-//        }
-//    }
-//
-//    private void nextStep() {
-//        synchronized (snapshotsLock) {
-//            int size = snapshots.size();
-//            snapshots.add(snapshots.get(size-1).next());
-//            if (size == PAST_STATES + 1 + FUTURE_STATES) {
-//                snapshots.remove(0);
-//            }
-//        }
-//    }
-//
     private Packet.Update buildFirstPacket(int[] snakeIds) {
         Packet.Update.Builder builder = Packet.Update.newBuilder();
         int id = 0;
@@ -130,60 +87,9 @@ public class SVMainGameState extends GameState implements InputProcessor {
         builder.setState(Packet.Update.PState.READY).setTimestamp(0).setVersion(0);
         return builder.build();
     }
-//
-//    private void processClientPacket(Packet.Update update) {
-//        // TODO: Implement Server reconciliation
-//        int step = update.getSnapshots(0).getStep();
-//        Packet.Update.PSnake pSnake = update.getSnapshots(0).getSnakes(0);
-//        synchronized (snapshotsLock) {
-//            int size = snapshots.size();
-//            int lastStep = snapshots.get(size-1).getStep();
-//            int index = size - lastStep + step - 1;
-//            if (index >= 0 && index < size) {
-//                Snake snake = snapshots.get(index).getSnakeById(pSnake.getId());
-//                if (pSnake.getInputId() > snake.INPUT_ID) {
-//                    snapshots.get(index).updateDirection(pSnake.getId(), pSnake.getDirection(), pSnake.getInputId());
-//                    if (pSnake.getDirection() != snake.DIRECTION) {
-//                        for (; index < size-1; ++index) {
-//                            snapshots.set(index+1, snapshots.get(index).next());
-//                        }
-//                    }
-//                }
-//            }
-//        }
-//    }
-//
-//    private void sendUpdate() {
-//        try {
-//            Packet.Update.Builder builder = Packet.Update.newBuilder();
-//            builder.setState(Packet.Update.PState.GAME_IN_PROGRESS);
-//            synchronized (snapshotsLock) {
-//                int size = snapshots.size();
-//                for (int i = 0; i < size - FUTURE_STATES; ++i) {
-//                    Snapshot snapshot = snapshots.get(i);
-//                    Set<Integer> keys = snapshot.getSnakeIds();
-//                    Packet.Update.PSnapshot.Builder snapshotBuilder = Packet.Update.PSnapshot.newBuilder();
-//                    snapshotBuilder.setStep(snapshot.getStep());
-//                    for (Integer k : keys) {
-//                        Snake s = snapshot.getSnakeById(k);
-//                        // TODO: remove
-//                        if (i == size - FUTURE_STATES - 1 && k.intValue() == 0) {
-//                            Gdx.app.debug(TAG, String.valueOf(s.DIRECTION));
-//                        }
-//                        snapshotBuilder.addSnakes(Packet.Update.PSnake.newBuilder().setId(s.ID).setInputId(s.INPUT_ID).setDirection(s.DIRECTION).build());
-//                    }
-//                    builder.addSnapshots(snapshotBuilder.build());
-//                }
-//            }
-//            _app.getAgent().send(builder.build());
-//        } catch (Exception e) {
-//            Gdx.app.error(TAG, "Error while sending update", e);
-//        }
-//    }
 
     @Override
     public void render(float delta) {
-//        nextStep();
         Gdx.gl.glClearColor(0, 0, 1, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
@@ -195,7 +101,6 @@ public class SVMainGameState extends GameState implements InputProcessor {
             builder.append(' ');
         }
         Gdx.app.debug(TAG, builder.toString());
-//        lastUpdateTime = System.nanoTime();
     }
 
     @Override
@@ -230,18 +135,6 @@ public class SVMainGameState extends GameState implements InputProcessor {
 
     @Override
     public boolean keyDown(int keycode) {
-//        Snapshot snapshot = getCurrentSnapshot();
-//        if (keycode == Input.Keys.LEFT) {
-//            snapshot.updateDirection(0, LEFT, inputId.incrementAndGet());
-//        } else if (keycode == Input.Keys.UP) {
-//            Gdx.app.debug(TAG, "Up pressed");
-//            snapshot.updateDirection(0, UP, inputId.incrementAndGet());
-//        } else if (keycode == Input.Keys.RIGHT) {
-//            snapshot.updateDirection(0, RIGHT, inputId.incrementAndGet());
-//        } else if (keycode == Input.Keys.DOWN) {
-//            snapshot.updateDirection(0, DOWN, inputId.incrementAndGet());
-//        }
-//        return true;
         Gdx.app.debug(TAG, "Keycode " + keycode + " pressed");
         if (keycode == Input.Keys.LEFT) {
             serverSnapshot.onServerInput(Constants.LEFT);
