2f37315cc967a4e3a946a6d8719486904640d110
Tony Zhang <tonyzhang617@gmail.com>
2018-07-24 01:05:34 -0400

    Add icon and change name

--- android/src/com/tianyi/zhang/multiplayer/snake/AndroidLauncher.java
+++ android/src/com/tianyi/zhang/multiplayer/snake/AndroidLauncher.java
@@ -7,10 +7,10 @@ import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
 import com.tianyi.zhang.multiplayer.snake.App;
 
 public class AndroidLauncher extends AndroidApplication {
-	@Override
-	protected void onCreate (Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
-		initialize(new App(), config);
-	}
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+        initialize(new App(), config);
+    }
 }
--- desktop/src/com/tianyi/zhang/multiplayer/snake/desktop/DesktopLauncher.java
+++ desktop/src/com/tianyi/zhang/multiplayer/snake/desktop/DesktopLauncher.java
@@ -6,10 +6,11 @@ import com.tianyi.zhang.multiplayer.snake.App;
 import com.tianyi.zhang.multiplayer.snake.helpers.Constants;
 
 public class DesktopLauncher {
-	public static void main (String[] arg) {
-		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		config.width = Constants.WINDOW_WIDTH;
-		config.height = Constants.WINDOW_HEIGHT;
-		new LwjglApplication(new App(), config);
-	}
+    public static void main(String[] arg) {
+        LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+        config.title = "snakeLANd";
+        config.width = Constants.WINDOW_WIDTH;
+        config.height = Constants.WINDOW_HEIGHT;
+        new LwjglApplication(new App(), config);
+    }
 }
